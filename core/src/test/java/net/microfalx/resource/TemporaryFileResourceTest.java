60224c2d78776bb851f6348405f732f460d1d794
Alex Tarau <alex@tarau.net>
2024-06-19 11:53:19 -0400

    Unit testing the TemporaryFileResource

--- core/src/test/java/net/microfalx/resource/TemporaryFileResourceTest.java
+++ core/src/test/java/net/microfalx/resource/TemporaryFileResourceTest.java
@@ -1,23 +1,60 @@
 package net.microfalx.resource;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 class TemporaryFileResourceTest {
 
     @Test
-    void file() {
+    void createFileWithPrefixAndSuffix() throws IOException {
+        Resource resource = TemporaryFileResource.file("test", "file");
+        Assertions.assertThat(resource.getFileName()).startsWith("test").endsWith("file");
+        assertNull(resource.getFileExtension());
+        assertEquals("application/octet-stream", resource.getMimeType());
+        assertTrue(resource.isFile());
+        assertFalse(resource.exists());
+        assertFalse(resource.isDirectory());
+        Assertions.assertThat(resource.getPath()).contains("/Temp/");
+    }
+
+    @Test
+    void createDirectoryWithPrefixAndSuffix() throws IOException {
+        Resource resource = TemporaryFileResource.directory("test", "directory");
+        Assertions.assertThat(resource.getFileName()).startsWith("test").endsWith("directory");
+        assertNull(resource.getFileExtension());
+        assertEquals("application/octet-stream", resource.getMimeType());
+        assertTrue(resource.isDirectory());
+        assertFalse(resource.exists());
+        assertFalse(resource.isFile());
+        Assertions.assertThat(resource.getPath()).contains("/Temp/");
+    }
+
+    @Test
+    void createFileWithFileName() {
         assertEquals("fixed.txt", TemporaryFileResource.file("fixed.txt").getFileName());
         assertEquals(Resource.Type.FILE, TemporaryFileResource.file("fixed.txt").getType());
         assertTrue(TemporaryFileResource.file("prefix", "suffix").getFileName().startsWith("prefix"));
     }
 
     @Test
-    void directory() {
+    void createDirectoryWithDirectoryName() {
         assertEquals("fixed.txt", TemporaryFileResource.directory("fixed.txt").getFileName());
         assertEquals(Resource.Type.DIRECTORY, TemporaryFileResource.directory("fixed.txt").getType());
         assertTrue(TemporaryFileResource.directory("prefix", "suffix").getFileName().startsWith("prefix"));
+    }
+
+    @Test
+    void createFileWithTypeAndFileName() throws IOException {
+        Resource resource = TemporaryFileResource.create(Resource.Type.FILE, "test");
+        assertEquals("test", resource.getFileName());
+        assertEquals("application/octet-stream", resource.getMimeType());
+        assertTrue(resource.isFile());
+        assertFalse(resource.exists());
+        assertFalse(resource.isDirectory());
+        Assertions.assertThat(resource.getPath()).contains("/Temp/");
     }
 }
